5e9516da5acab9e102ff599d52d7cf052a3c1fbd
Wilde Chu <wildechu23@gmail.com>
2020-10-23 12:54:59 -0400

    main added and tests done

--- Tester.java
+++ Tester.java
@@ -1,3 +1,14 @@
+import java.util.Arrays;
+
 public class Tester {
-  
+  public static void main(String[] args) {
+    int[][] A = { 
+      { 1, 0, 12, -1 }, 
+      { 7, -2, 2, 1 }, 
+      { -5, -2, 2, -9 } 
+    };
+    int[] B = { 1, 3, 5 };
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+  }
 }
