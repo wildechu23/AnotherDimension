1b2465ae7363b41260f5ba9d2de96276d1fb2932
Wilde Chu <wildechu23@gmail.com>
2020-10-23 12:48:11 -0400

    largest done

--- ArrayOps.java
+++ ArrayOps.java
@@ -6,4 +6,14 @@ public class ArrayOps {
     }
     return sum;
   }
+
+  public static int largest(int[] arr) {
+    int l = 0;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > l) {
+        l = arr[i];
+      }
+    }
+    return l;
+  }
 }
